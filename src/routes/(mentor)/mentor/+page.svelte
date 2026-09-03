ae5f8f3f1feeac1ff07fd80c654531532878c062
Jian <jian@nesaga.sch.id>
2026-09-03 19:01:32 +0000

    style: seragamkan ukuran ikon SVG (14x14) dan label tombol aksi header seluruh halaman mentor sesuai standar /mentor/pertemuan

--- src/routes/(mentor)/mentor/+page.svelte
+++ src/routes/(mentor)/mentor/+page.svelte
@@ -71,6 +71,7 @@
 						<line x1="12" y1="5" x2="12" y2="19" />
 						<line x1="5" y1="12" x2="19" y2="12" />
 					</svg>
+					<span>Kelola Pertemuan</span>
 				</a>
 			{/snippet}
 		</PageHeaderCard>
