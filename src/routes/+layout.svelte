9a36bf8f8dfec89b18a55be035534d95a7bdee99
Jian <jian@nesaga.sch.id>
2026-09-16 16:21:17 +0000

    fix(assets): update favicon import path to favicon.ico

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import { navigating } from '$app/stores';
 	import ToastContainer from '$lib/components/ui/ToastContainer.svelte';
 
